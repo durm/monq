aefc2013ddc67aa19a2a9805a6956cc83a4f0d29
Alex Korotkov <durm.icecoffee@gmail.com>
2015-04-03 00:16:03 +0300

    check

--- visits/urls.py
+++ visits/urls.py
@@ -5,5 +5,9 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^start/$', 'visits.views.start', name='start_visit'),
     url(r'^current/$', 'visits.views.current', name='go_to_current_visit'),
+    url(r'^visit/(?P<id>[0-9]+)/$', 'visits.views.go_to_visit', name='go_to_visit'),
+    #(?P<num>[0-9]+)
     url(r'^close/$', 'visits.views.close', name='close_visit'),
+    url(r'^check/start/$', 'visits.views.start_check', name='start_check'),
+    url(r'^check/(?P<id>[0-9]+)/$', 'visits.views.go_to_check', name='go_to_check'),
 )
--- visits/views.py
+++ visits/views.py
@@ -39,4 +39,32 @@ def close(request):
     current_visit.close()
     current_visit.save()
     
-    return render_to_response("visit.html", {"visit":current_visit})
+    return redirect(reverse("go_to_visit", args=[current_visit.id]))
+
+def go_to_visit(request, id):
+    visit = Visit.objects.get(id=id)
+    return render_to_response("visit.html", {"visit":visit})
+
+def start_check(request):
+
+    current_visit = Visit.get_opened_session(request.user)
+    if current_visit is None :
+        return redirect(reverse("landing"))
+
+    check = Check.objects.create()
+    check.save()
+    current_visit.checks.add(check)
+
+    return redirect(reverse("go_to_check", args=[check.id]))
+
+def go_to_check(request,id):
+
+    current_visit = Visit.get_opened_session(request.user)
+    if current_visit is None :
+        return redirect(reverse("landing"))
+
+    check = Check.objects.get(id=id)
+
+    assert current_visit.checks.filter(id=check.id).first() is not None, "Check is not in session"
+
+    return render_to_response("current_check.html", {"check":check})
